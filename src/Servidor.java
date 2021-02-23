a3545b252cfb0cf137c5532ecaeac94ae82826d7
PaSalva <pablo.d.salva@gmail.com>
2021-02-23 12:58:32 +0100

    Arreglo mediocre pero semifuncional. Solo el server muestra los mensajes de todo el mundo

--- src/Servidor.java
+++ src/Servidor.java
@@ -22,16 +22,17 @@ public class Servidor {
 	    System.out.println("Servidor conectado. Esperando conexiones. ");
 	    serverSocket = new ServerSocket(PORT, 4);
 	    while (true) {
-	    socket = serverSocket.accept();
-	    System.out.println("Cliente conectado. Disfruten de la comunicación");
-	    din = new DataInputStream(socket.getInputStream());
-	    dout = new DataOutputStream(socket.getOutputStream());
-	    br = new BufferedReader(new InputStreamReader(System.in));
+		socket = serverSocket.accept();
+		System.out.println("Cliente conectado. Disfruten de la comunicación");
+		new Thread(new Control(socket)).start();
+		//din = new DataInputStream(socket.getInputStream());
+		//dout = new DataOutputStream(socket.getOutputStream());
+		//br = new BufferedReader(new InputStreamReader(System.in));
 
-	    String sMensajeRecibido = "", sMensajeEnviado = "";
-	    
-		sMensajeRecibido = din.readUTF();
-		System.out.println("client says: " + sMensajeRecibido);
+		//String sMensajeRecibido = "", sMensajeEnviado = "";
+
+//		sMensajeRecibido = din.readUTF();
+//		System.out.println("client says: " + sMensajeRecibido);
 //		sMensajeEnviado = br.readLine();
 //		dout.writeUTF(sMensajeEnviado);
 //		dout.flush();
