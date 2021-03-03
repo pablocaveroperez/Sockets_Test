c31caaa4d19f240d7cda0d8cc90c7814943b5747
PaSalva <pablo.d.salva@gmail.com>
2021-03-03 19:42:07 +0100

    prueba de cavero para descomitear

--- src/Cliente.java
+++ src/Cliente.java
@@ -8,45 +8,51 @@ import javax.net.ssl.SSLSocketFactory;
 
 public class Cliente {
 
-	private static final String HOST = "localhost";
+    private static final String HOST = "localhost";
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		DataOutputStream dout = null;
-		BufferedReader br = null;
-		Socket socket = null;
-		int idCliente = Servidor.iContador++;
-		System.setProperty("javax.net.ssl.trustStore", "certs/clientTrustedCerts.jks");
-		System.setProperty("javax.net.ssl.trustStorePassword", "servpass");
-		
+	DataOutputStream dout = null;
+	BufferedReader br = null;
+	Socket socket = null;
+	int idCliente = Servidor.iContador++;
+	System.setProperty("javax.net.ssl.trustStore", "certs/clienteTrustedCerts.jks");
+	System.setProperty("javax.net.ssl.trustStorePassword", "medacmedac");
 
+	try {
+	    System.out.println("El cliente se va a conectar");
+	    SSLSocketFactory clientFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+	    socket = clientFactory.createSocket(HOST, Servidor.PORT);
+
+	    // socket = new Socket(HOST, Servidor.PORT);
+	    Servidor.ControlCliente hilo = new Servidor.ControlCliente(socket);
+	   new Thread(hilo).start();
+	    //hilo.start();
+	    dout = new DataOutputStream(socket.getOutputStream());
+	    br = new BufferedReader(new InputStreamReader(System.in));
+	    String sMensajeEnviado = "";
+	    while (!sMensajeEnviado.equals("gitano")) {
+		sMensajeEnviado = br.readLine();
+		dout.writeUTF("# " + idCliente + " : " + sMensajeEnviado);
+	    }
+	    System.out.println("Adios, por racista");
+	    hilo.cerrar();
+
+	} catch (Exception ex) {
+
+	    if (socket != null) {
 		try {
-			System.out.println("El cliente se va a conectar");
-			SSLSocketFactory clientFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-			socket = clientFactory.createSocket(HOST, Servidor.PORT);
-			
-			//socket = new Socket(HOST, Servidor.PORT);
-			new Thread(new Servidor.ControlCliente(socket)).start();
-			dout = new DataOutputStream(socket.getOutputStream());
-			br = new BufferedReader(new InputStreamReader(System.in));
-			String sMensajeEnviado = "";
-			while (!sMensajeEnviado.equals("gitano")) {
-				sMensajeEnviado = br.readLine();
-				dout.writeUTF("# " + idCliente + " : " + sMensajeEnviado);
-			}
-
-		} catch (Exception ex) {
-			try {
-				socket.close();
-				br.close();
-				dout.close();
-			} catch (IOException e) {
-				System.out.println("No se ha podido conectar con el servidor");
-			}
-			ex.printStackTrace();
-			System.err.println("Error." + ex.getMessage());
+		    socket.close();
+		    br.close();
+		    dout.close();
+		} catch (IOException e) {
+		    System.err.println(e.getMessage());
 		}
+	    }
 
+	    System.err.println("Error." + ex.getMessage());
 	}
+
+    }
 
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -12,52 +12,37 @@ public class Servidor {
     public static final int PORT = 1234;
     public static ArrayList<ObjectOutputStream> clienteStream = new ArrayList<ObjectOutputStream>();
     public static int iContador = 0;
+    private static ServerSocket serverSocket = null;
+    private static Socket socket = null;
+    
 
     /**
-     * Creacion de certificados: keytool -genkey -keyalg RSA -alias serverKey
-     * -keystore serverKey.jks -storepass servpass
+     * Creacion de certificados: 
+     * keytool -genkey -keyalg RSA -alias serverKey -keystore serverKey.jks -storepass servpass
      * 
-     * Creacion de certificado de servidor keytool -export -keystore serverkey.jks
-     * -alias serverKey -file ServerPublicKey.cer (Contraseña es servpass)
+     * Creacion de certificado de servidor keytool -export -keystore serverkey.jks -alias serverKey -file ServerPublicKey.cer (Contraseña es servpass)
      * 
-     * Creacion de certificado para cliente keytool -import -alias serverKey -file
-     * ServerPublicKey.cer -keystore clienteTrustedCerts.jks -keypass clientpass
-     * -storepass servpass
+     * Creacion de certificado para cliente keytool -import -alias serverKey -file ServerPublicKey.cer -keystore clienteTrustedCerts.jks -keypass clientpass -storepass servpass
      * 
      * @param args
+     * @throws Exception 
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	System.setProperty("javax.net.ssl.keyStore", "certs/serverKey.jks");
-	System.setProperty("javax.net.ssl.keyStorePassword", "servpass");
-	System.setProperty("javax.net.ssl.trustStore", "certs/clientTrustedCerts.jks");
-	System.setProperty("javax.net.ssl.trustStorePassword", "servpass");
-	ServerSocket serverSocket = null;
-	Socket socket = null;
+	System.setProperty("javax.net.ssl.keyStorePassword", "medacmedac");
+	System.setProperty("javax.net.ssl.trustStore", "certs/clienteTrustedCerts.jks");
+	System.setProperty("javax.net.ssl.trustStorePassword", "medacmedac");
+
 
 	try {
-
-	    SSLServerSocketFactory serverFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-	    //serverSocket = serverFactory.createServerSocket(PORT, 4);
-
-	    serverSocket = new ServerSocket(PORT, 4);
-	    System.out.println("Servidor conectado. Esperando conexiones. ");
-
-	    while (true) {
-		socket = serverSocket.accept();
-		ObjectOutputStream objOutStream = new ObjectOutputStream(socket.getOutputStream());
-		clienteStream.add(objOutStream);
-		System.out.println("Cliente conectado. Disfruten de la comunicación");
-		Thread t = new Thread(new Control(socket, objOutStream));
-		t.start();
-
-	    }
+	    metodoCutreCavero();
+	    
 
 	} catch (Exception ex) {
+	    metodoCutreCavero();
 
-	    ex.printStackTrace();
-
-	    System.err.println("Error." + ex.getMessage());
+	    //System.err.println("Error." + ex.getMessage());
 	} finally {
 	    if (socket != null) {
 		try {
@@ -69,6 +54,24 @@ public class Servidor {
 	    }
 	}
 
+    }
+    
+    private static void metodoCutreCavero() throws Exception {
+	SSLServerSocketFactory serverFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+	    serverSocket = serverFactory.createServerSocket(PORT, 4);
+
+	    //serverSocket = new ServerSocket(PORT, 4);
+	    System.out.println("Servidor conectado. Esperando conexiones. ");
+
+	    while (true) {
+		socket = serverSocket.accept();
+		ObjectOutputStream objOutStream = new ObjectOutputStream(socket.getOutputStream());
+		clienteStream.add(objOutStream);
+		System.out.println("Cliente conectado. Disfruten de la comunicación");
+		Thread t = new Thread(new Control(socket, objOutStream));
+		t.start();
+
+	    }
     }
 
     public static int getNewID() {
@@ -95,6 +98,10 @@ public class Servidor {
 	public ControlCliente(Socket clientSocket) {
 	    this.clientSocket = clientSocket;
 	}
+	
+	public void cerrar() {
+	    clienteStream.remove(clientSocket);
+	}
 
 	@Override
 	public void run() {
@@ -112,7 +119,7 @@ public class Servidor {
 		}
 	    } catch (IOException e) {
 		System.out.println(clientSocket.getInetAddress().getHostAddress() + " disconnected from the Server");
-		clienteStream.remove(clientSocket);
+		cerrar();
 	    }
 	}
     }
